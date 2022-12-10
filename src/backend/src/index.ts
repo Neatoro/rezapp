e030acfecf1a0c74f9abfcf800a781385fb42eed
Moritz Schramm <moritz.schramm@gmx.de>
2022-12-10 23:35:19 +0100

    Add basic backend setup

---
+++ src/backend/src/index.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app/app.module";
+
+async function bootstrap() {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    const port = process.env.PORT || 8080;
+
+    await app.listen(port);
+}
+bootstrap();
