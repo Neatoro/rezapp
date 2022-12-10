ea28160051e99050527daeb1f26dc59faf4f5a85
Moritz Schramm <moritz.schramm@gmx.de>
2022-12-10 23:42:39 +0100

    Add prettier

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { AppModule } from "./app/app.module";
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
