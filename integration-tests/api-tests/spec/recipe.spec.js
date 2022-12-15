3e92f5147b6544a54b61523542d28716fffa6112
Moritz Schramm <moritz.schramm@gmx.de>
2022-12-15 16:21:24 +0100

    Add tests for recipe creation

--- integration-tests/api-tests/spec/recipe.spec.js
+++ integration-tests/api-tests/spec/recipe.spec.js
@@ -114,5 +114,382 @@ describe('Recipe', () => {
                 error: 'Bad Request'
             });
         });
+
+        it('should fail if name is not provided', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    description: 'Test'
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: ['name must be a string', 'name should not be empty'],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if name is not a string', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: true,
+                    description: 'Test'
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: ['name must be a string'],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if name is not provided', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test'
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: [
+                    'description must be a string',
+                    'description should not be empty'
+                ],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if name is not a string', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: true
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: ['description must be a string'],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if steps are not strings', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    steps: ['Test', true]
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: ['each value in steps must be a string'],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if ingredient id is not provided', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    ingredients: [
+                        {
+                            amount: 3,
+                            unit: 'g'
+                        }
+                    ]
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: [
+                    'ingredients.0.ingredient must be a string',
+                    'ingredients.0.ingredient should not be empty'
+                ],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if ingredient id is not a string', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    ingredients: [
+                        {
+                            ingredient: true,
+                            amount: 3,
+                            unit: 'g'
+                        }
+                    ]
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: ['ingredients.0.ingredient must be a string'],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if ingredient amount is not a number', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    ingredients: [
+                        {
+                            ingredient: 'Test',
+                            amount: '3',
+                            unit: 'g'
+                        }
+                    ]
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: [
+                    'ingredients.0.amount must be a number conforming to the specified constraints'
+                ],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should fail if ingredient unit is not a string', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    ingredients: [
+                        {
+                            ingredient: 'Test',
+                            amount: 3,
+                            unit: true
+                        }
+                    ]
+                })
+            });
+            const data = await response.json();
+
+            expect(response.status).toBe(400);
+            expect(data).toEqual({
+                statusCode: 400,
+                message: ['ingredients.0.unit must be a string'],
+                error: 'Bad Request'
+            });
+        });
+
+        it('should create a recipe without ingredients and steps', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test'
+                })
+            });
+            const data = await response.json();
+            const { id, ...exceptId } = data;
+
+            expect(response.status).toBe(201);
+            expect(id).toMatch(
+                /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+            );
+            expect(exceptId).toEqual({
+                name: 'Test',
+                description: 'Test',
+                steps: [],
+                ingredients: [],
+                image: false
+            });
+        });
+
+        it('should create a recipe without ingredients', async () => {
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    steps: ['Foo bar']
+                })
+            });
+            const data = await response.json();
+            const { id, ...exceptId } = data;
+
+            expect(response.status).toBe(201);
+            expect(id).toMatch(
+                /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+            );
+            expect(exceptId).toEqual({
+                name: 'Test',
+                description: 'Test',
+                steps: [
+                    jasmine.objectContaining({
+                        description: 'Foo bar',
+                        rank: 0
+                    })
+                ],
+                ingredients: [],
+                image: false
+            });
+        });
+
+        it('should create a recipe without steps', async () => {
+            await profileHelper.apply('test-ingredient');
+
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    ingredients: [
+                        {
+                            ingredient: '6961db05-7a8a-40d8-ad9b-0873dc23e271',
+                            amount: 200,
+                            unit: 'g'
+                        }
+                    ]
+                })
+            });
+            const data = await response.json();
+            const { id, ...exceptId } = data;
+
+            expect(response.status).toBe(201);
+            expect(id).toMatch(
+                /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+            );
+            expect(exceptId).toEqual({
+                name: 'Test',
+                description: 'Test',
+                steps: [],
+                ingredients: [
+                    jasmine.objectContaining({
+                        ingredient: {
+                            id: '6961db05-7a8a-40d8-ad9b-0873dc23e271'
+                        },
+                        amount: 200,
+                        unit: 'g'
+                    })
+                ],
+                image: false
+            });
+        });
+
+        it('should create a recipe with ingredients and steps', async () => {
+            await profileHelper.apply('test-ingredient');
+
+            const response = await fetch('http://localhost:8080/recipe', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: 'Test',
+                    description: 'Test',
+                    steps: ['Foo bar'],
+                    ingredients: [
+                        {
+                            ingredient: '6961db05-7a8a-40d8-ad9b-0873dc23e271',
+                            amount: 200,
+                            unit: 'g'
+                        }
+                    ]
+                })
+            });
+            const data = await response.json();
+            const { id, ...exceptId } = data;
+
+            expect(response.status).toBe(201);
+            expect(id).toMatch(
+                /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+            );
+            expect(exceptId).toEqual({
+                name: 'Test',
+                description: 'Test',
+                steps: [
+                    jasmine.objectContaining({
+                        description: 'Foo bar',
+                        rank: 0
+                    })
+                ],
+                ingredients: [
+                    jasmine.objectContaining({
+                        ingredient: {
+                            id: '6961db05-7a8a-40d8-ad9b-0873dc23e271'
+                        },
+                        amount: 200,
+                        unit: 'g'
+                    })
+                ],
+                image: false
+            });
+        });
     });
 });
