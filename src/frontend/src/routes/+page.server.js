bceed3f7a6275bef961e11d5112e99b53bf8aaf0
Moritz Schramm <moritz.schramm@sap.com>
2023-01-02 16:18:21 +0100

    Filter by label in frontend

--- src/frontend/src/routes/+page.server.js
+++ src/frontend/src/routes/+page.server.js
@@ -1,13 +1,42 @@
 import { env } from '$env/dynamic/private';
 
-export async function load({ fetch, url }) {
+function getSelectedLabels({ url }) {
+    const query = url.searchParams;
+    const labelsQuery = query.get('labels');
+    const labels = labelsQuery ? labelsQuery.split(',') : [];
+    return labels;
+}
+
+async function loadRecipes({ fetch, url }) {
     const query = url.searchParams;
     const search = query.get('search') || '';
 
+    const labels = getSelectedLabels({ url });
+
     const response = await fetch(
-        `${env.BACKEND_URL}/api/recipe?search=${encodeURIComponent(search)}`
+        `${env.BACKEND_URL}/api/recipe?search=${encodeURIComponent(
+            search
+        )}&labels=${encodeURIComponent(labels.join(','))}`
     );
     const data = await response.json();
+    return data.recipes;
+}
 
-    return data;
+async function loadLabels({ fetch }) {
+    const response = await fetch(`${env.BACKEND_URL}/api/label`);
+    const data = await response.json();
+
+    return data.labels;
+}
+
+export async function load({ fetch, url }) {
+    const labels = await loadLabels({ fetch });
+    const recipes = await loadRecipes({ fetch, url });
+
+    return {
+        recipes,
+        labels,
+        selectedLabels: getSelectedLabels({ url }),
+        search: url.searchParams.get('search') || ''
+    };
 }
