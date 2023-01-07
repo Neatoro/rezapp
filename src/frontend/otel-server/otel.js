2a111d6198be916f5b765a6cabba74ec8aa74834
Moritz Schramm <moritz.schramm@gmx.de>
2023-01-07 20:47:52 +0100

    Proper tracing in frontend

--- src/frontend/otel-server/otel.js
+++ src/frontend/otel-server/otel.js
@@ -1,13 +1,14 @@
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { Resource } from '@opentelemetry/resources';
 import {
     BatchSpanProcessor,
     NodeTracerProvider
 } from '@opentelemetry/sdk-trace-node';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import {
+    SemanticAttributes,
+    SemanticResourceAttributes
+} from '@opentelemetry/semantic-conventions';
+import { SpanKind, context, propagation } from '@opentelemetry/api';
 
 const provider = new NodeTracerProvider({
     resource: new Resource({
@@ -22,16 +23,43 @@ provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
 provider.register();
 
-registerInstrumentations({
-    instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()]
-});
-
 async function bootstrap() {
     const { handler } = await import('./handler.js');
     const { default: express } = await import('express');
 
     const app = express();
-    app.use(handler);
+
+    const otelHandler = async (request, response) => {
+        const tracer = provider.getTracer('frontend');
+        const newContext = propagation.extract(
+            context.active(),
+            request.headers
+        );
+
+        const span = tracer.startSpan(
+            `${request.method} ${request.path}`,
+            {
+                kind: SpanKind.SERVER
+            },
+            newContext
+        );
+        span.setAttribute(SemanticAttributes.HTTP_METHOD, request.method);
+        request.headers['traceparent'] = `00-${span.spanContext().traceId}-${
+            span.spanContext().spanId
+        }-${span
+            .spanContext()
+            .traceFlags.toString(16)
+            .padStart(2, '0')}`.toLowerCase();
+
+        console.log(request.headers['traceparent']);
+
+        handler(request, response);
+        response.once('finish', () => {
+            span.end();
+        });
+    };
+
+    app.use(otelHandler);
     app.listen(3000, () => console.log('started on port 3000'));
 }
 bootstrap();
